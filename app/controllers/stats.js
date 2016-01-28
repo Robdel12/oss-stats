90b4fc93daf326300665ef1e44155fde27166827
Robdel12 <robertdeluca19@gmail.com>
2016-01-27 23:53:52 -0600

    Add basic sorting

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   range: "week",
-  queryParams: ['range']
+  queryParams: ['range'],
+  sortDirection: 'desc',
+  sortProperties: ['downloads:desc'],
+  sortedModel: Ember.computed.sort('model', 'sortProperties'),
+
+  actions: {
+    sort(column) {
+      this.get('sortDirection') === "desc" ? this.set('sortDirection', "asc") : this.set('sortDirection', "desc");
+
+      this.set("sortProperties", [`${column}:${this.get("sortDirection")}`]);
+    }
+  }
 });
