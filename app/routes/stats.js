1c21545a088b577782cb5505013d3a14277bf17a
Robdel12 <robertdeluca19@gmail.com>
2016-02-12 21:07:16 -0600

    Add emberx-range-input

--- app/routes/stats.js
+++ app/routes/stats.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-const PACKAGE_LIST = "emberx-select,emberx-file-input,ember-impagination,ember-promise-helpers,emberx-file-reader,emberx-select-blockless,ember-introjs,virtual-each";
+const PACKAGE_LIST = "emberx-select,emberx-file-input,ember-impagination,ember-promise-helpers,emberx-file-reader,emberx-select-blockless,ember-introjs,virtual-each,emberx-range-input";
 
 export default Ember.Route.extend({
   queryParams: {
